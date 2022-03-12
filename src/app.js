21bbcd9a71b90c91c5542e5b8ffffb5508b1ab86
Dan Burton <danburton720@hotmail.com>
2022-03-12 11:36:41 +0000

    deploy to heroku

--- src/app.js
+++ src/app.js
@@ -83,22 +83,17 @@ passport.use(new GoogleStrategy({
         }
     }));
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static("./client/build"));
-//     // const path = require("path");
-//     // app.get("*", (req, res) => {
-//     //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     // })
-// }
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
 }
 
 app.use(authRouter);
 app.use(userRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
 
 module.exports = app;
